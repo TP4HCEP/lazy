4e8435f5828fd63320b5ad3e1c318245f271734a
TP4HCEP <123848993+TP4HCEP@users.noreply.github.com>
2023-01-30 04:54:57 +0300

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+# Kernel Repo
+KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/TP4HCEP/RevengeOS
+
+# Kernel Branch
+KERNEL_BRANCH=r11.0
+
+# The name of the device for which the kernel is built
+MODEL="pyxis"
+
+# The codename of the device
+DEVICE="pyxis"
+
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=pyxis_defconfig
+
+# Show manufacturer info
+MANUFACTURERINFO="Xiaomi"
+
+# tambahkan changelog di anykernel
+CHANGELOGS=n
+
+# opsi spectrum "y" atau "n"
+SPECTRUM=n
+
+# Kernel Variant
+
+HMP=n
+
+NAMA=Signature
+
+JENIS=IV
+
+VARIAN=r3
+
+# Build Type
+BUILD_TYPE="nh"
+
+# Specify compiler.
+# 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
+COMPILER="linaro"
+
+# Message on anykernel when installatio
+MESSAGE="just flash and forget"
+
+# KBUILD ENV
+K_USER=TP4HCEP
+K_HOST=Github
+K_VERSION=1
+
+
+# arch & subarch
+K_ARCH=arm64
+K_SUBARCH=arm64
