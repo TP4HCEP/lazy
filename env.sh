24bc955e317478858d7e7ae5f5722c816fbdd4a5
TPAHCEP <123848993+TP4HCEP@users.noreply.github.com>
2023-02-22 21:14:22 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
-
-# Kernel Repo
-KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/TP4HCEP/RevengeOS
+KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/TP4HCEP/tkilla
 
 # Kernel Branch
-KERNEL_BRANCH=r11.0
+KERNEL_BRANCH=test
 
 # The name of the device for which the kernel is built
 MODEL="pyxis"
