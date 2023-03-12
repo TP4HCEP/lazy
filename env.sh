7c34e1c7cac162eff1bd9d0023d10fda372e112f
TPAHCEP <123848993+TP4HCEP@users.noreply.github.com>
2023-03-13 02:34:47 +0300

    pyxi

--- env.sh
+++ env.sh
@@ -14,7 +14,8 @@ DEVICE="pyxis"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=pyx_lastgood_defconfig
+DEFCONFIG=pyxi_defconfig
+#pyx_lastgood_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="Xiaomi"
