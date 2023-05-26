2070fbf0bb7c4a6827da58ab2efc70b21fae088b
TPAHCEP <123848993+TP4HCEP@users.noreply.github.com>
2023-05-26 05:03:08 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -40,7 +40,7 @@ BUILD_TYPE="nh"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER="clang"
+COMPILER="clangxgcc"
 
 # Message on anykernel when installatio
 MESSAGE="just flash and forget"
